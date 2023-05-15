7afda227ecdbd1421b3d1954c849a82733eddb40
Mohamed D <simoforever2018@gmail.com>
2023-05-15 01:00:36 +0100

    migrations and models added

---
+++ Ecommerce/database/migrations/2023_05_14_235109_create_livraisons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('livraisons', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime("date");
+            $table->foreignId('commande_id')->constrained('commande_id','id')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('mode_livraison_id')->constrained('mode_livraison_id','id')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('livraisons');
+    }
+};
