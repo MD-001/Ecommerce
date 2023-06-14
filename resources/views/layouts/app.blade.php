9a444baf36459f8734a8d6254504d34678ea102c
amine1nhili <amine1nhili@gmail.com>
2023-06-14 19:47:48 +0100

    loginAndRegister

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,7 +95,7 @@
 								</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link" href="">
+								<a class="nav-link" href="{{ route('register') }}">
 									<button class="btn btn-outline-success">
 										<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person"
 											viewBox="0 0 16 16">
