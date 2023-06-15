ca69649149e37e401542454ccea9caf459e350cc
Mohamed D <simoforever2018@gmail.com>
2023-06-15 22:06:06 +0100

    css changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -461,7 +461,8 @@
 		//scroll to-top 
 		const toTop = document.querySelector(".to-top");
 		window.addEventListener("scroll", () => {
-			if (window.pageYOffset > 100) {
+			if (window.pageYOffset > 100 && window.pageYOffset + window.innerHeight < document.documentElement
+				.scrollHeight - 100) {
 				toTop.classList.add("active");
 			} else {
 				toTop.classList.remove("active");
